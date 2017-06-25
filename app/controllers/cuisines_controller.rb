22e79d5816b84832da82a3685c9896a03e279f45
Rogerio Bispo <rogerio_pd@yahoo.com.br>
2017-06-25 14:52:03 -0300

    Administrador Cadastra cozinha

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -5,9 +5,13 @@ class CuisinesController < ApplicationController
   end
 
   def create
-    @cuisine = Cuisine.create(name: params[:cuisine][:name])
-    redirect_to (cuisine_path(@cuisine.id)) and return if @cuisine.valid?
-    render template: "cuisines/errors.html.erb"
+    @cuisine = Cuisine.new(name: params[:cuisine][:name])
+    if @cuisine.save
+      redirect_to @cuisine
+    else
+      flash[:error] = @cuisine.errors.messages[:name]
+      render :new
+    end
   end
 
   def show
